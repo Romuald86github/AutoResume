0d99572e3a3088cbb0ddfe2c18aed9035fa6031d
Romuald86github <123880565+Romuald86github@users.noreply.github.com>
2024-06-10 16:30:26 +0000

    update

--- src/modeling/model_training.py
+++ src/modeling/model_training.py
@@ -53,6 +53,9 @@ if __name__ == "__main__":
             resume_vectors = data['resume_vectors']
             jd_vectors = data['jd_vectors']
 
+            print("resume_vectors shape:", resume_vectors.shape)
+            print("jd_vectors shape:", jd_vectors.shape)
+
             # Compute the cosine similarity between each resume and each job description
             similarity_matrix = cosine_similarity(resume_vectors, jd_vectors.T)
 
