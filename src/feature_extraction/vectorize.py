ae702259b622ba2d37f2423357719ffd2f7acb68
Romuald86github <123880565+Romuald86github@users.noreply.github.com>
2024-06-10 12:47:10 +0000

    updating vectorize.py

--- src/feature_extraction/vectorize.py
+++ src/feature_extraction/vectorize.py
@@ -3,19 +3,42 @@ import joblib
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 def vectorize_text(input_dir, output_file):
-    documents = []
+    resume_texts = []
+    resume_filenames = []
+    jd_texts = []
+    jd_filenames = []
+
     for filename in os.listdir(input_dir):
         if filename.endswith(".txt"):
             file_path = os.path.join(input_dir, filename)
             with open(file_path, 'r') as file:
                 text = file.read()
-            documents.append(text)
 
-    vectorizer = TfidfVectorizer()
-    vectors = vectorizer.fit_transform(documents)
+            if filename.startswith("resume_"):
+                resume_texts.append(text)
+                resume_filenames.append(filename)
+            elif filename.startswith("jd_"):
+                jd_texts.append(text)
+                jd_filenames.append(filename)
+
+    resume_vectorizer = TfidfVectorizer()
+    resume_vectors = resume_vectorizer.fit_transform(resume_texts)
+
+    jd_vectorizer = TfidfVectorizer()
+    jd_vectors = jd_vectorizer.fit_transform(jd_texts)
+
     with open(output_file, 'wb') as file:
-        joblib.dump({'raw_texts': documents, 'vectorizer': vectorizer}, file)
+        joblib.dump({
+            'resume_texts': resume_texts,
+            'resume_vectors': resume_vectors,
+            'resume_filenames': resume_filenames,
+            'jd_texts': jd_texts,
+            'jd_vectors': jd_vectors,
+            'jd_filenames': jd_filenames,
+            'resume_vectorizer': resume_vectorizer,
+            'jd_vectorizer': jd_vectorizer
+        }, file)
 
 if __name__ == "__main__":
-    vectorize_text('data/processed/preprocessed_resumes', 'data/resume_vectors.pkl')
-    vectorize_text('data/processed/preprocessed_job_descriptions', 'data/job_description_vectors.pkl')
+    vectorize_text('data/processed/preprocessed_resumes', 'data/vectors.pkl')
+    vectorize_text('data/processed/preprocessed_job_descriptions', 'data/vectors.pkl')
